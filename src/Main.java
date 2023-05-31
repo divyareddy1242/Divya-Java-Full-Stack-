59ca0f8da1bb438ff2dc47a5cc6722b9737dd5d7
Divya <divyareddy7390@gmail.com>
2023-05-31 15:49:41 -0500

    changes made

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
+<<<<<<< HEAD
         System.out.println("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
@@ -13,5 +14,18 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             //System.out.println("i = " + i);
       //  }
+=======
+       // System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+       // for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+          //  System.out.println("i = " + i);
+        Student s=new Student();
+        System.out.println("before name:"+s.getName());
+        System.out.println("Updated name:"+s.setName("Divya"));
+>>>>>>> a6fc8b0 (changes made)
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+     String Name="Vikranth";
+    public String getName() {
+        return Name;
+    }
+
+    public String setName(String n) {
+        Name = n;
+        return Name;
+    }
+}
