597d879f4818712208cc9822c2e2742e00317673
Divya <divyareddy7390@gmail.com>
2023-05-31 00:46:59 -0500

    changes added

---
+++ src/Male.java
@@ -0,0 +1,8 @@
+public class Male extends Person{
+    public void run()
+    {
+        super.run();
+        System.out.println("This is the sub class extending parent-class");
+    }
+}
+
---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+
+    public double bmi(float x,float y)
+    {
+        return (x)/(y*y);
+    }
+    public void run()
+    {
+        System.out.println("This is the Parent class of Male");
+    }
+}
+
