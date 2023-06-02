e32f7455477a9b08a0babef5de85462d916f9646
Divya <divyareddy7390@gmail.com>
2023-06-02 11:10:17 -0500

    commited all

---
+++ Day3-Que3/src/Animal.java
@@ -0,0 +1,7 @@
+
+public class Animal {
+    public void display()
+    {
+        System.out.println("Parent class is Animal");
+    }
+}
---
+++ Day3-Que3/src/Tiger.java
@@ -0,0 +1,6 @@
+public class Tiger extends Animal{
+    public void display()
+    {
+        System.out.println("This is the sub class of the Parent class is Animal");
+    }
+}
