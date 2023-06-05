e0cb3bccd6099b2e5cbd1c29593cb64aaf9afedf
Divya <divyareddy7390@gmail.com>
2023-06-05 00:58:03 -0500

    commit this file

---
+++ Staticque/src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static int rollno;
+    static
+    {
+        rollno=1242;
+    }
+    public static int display() {
+        return rollno;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Main.display());
+    }
+}
