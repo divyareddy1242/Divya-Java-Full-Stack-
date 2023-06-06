3c8f6df067061b0f9214bab239ce8804f5ee8a53
Divya <divyareddy7390@gmail.com>
2023-06-05 20:30:10 -0500

    commit all

---
+++ Reversenum/src/Main.java
@@ -0,0 +1,27 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+       // System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        //for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+           // System.out.println("i = " + i);
+        //}
+        int num=123456;
+        while(num>0)
+        {
+            //int num1=num;
+            int remainder=num%10;
+            System.out.print(remainder);
+            num=num/10;
+
+
+        }
+    }
+}
