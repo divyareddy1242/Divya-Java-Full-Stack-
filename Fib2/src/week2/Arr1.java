975136e8af193c3041cb8a5f0e7a3c27a20c7deb
Divya <divyareddy7390@gmail.com>
2023-06-09 11:24:28 -0500

    commit this file

--- Fib2/src/week2/Arr1.java
+++ Fib2/src/week2/Arr1.java
@@ -1,13 +1,20 @@
 package week2;
 
 public class Arr1 {
-    public static void main(String args[])
-    {
-        int a[]=new int[]{10,20,5,60,9,100};
-        System.out.println("Array Reverse");
-        for(int i=a.length-1;i>=0;i--)
-        {
-            System.out.print(a[i]+" " );
-        }
-    }
+    static int a[] = {10,20,30,40,50};
+
+            static void reverse() {
+                System.out.println("Reversed Array");
+                int newArr[] = new int[a.length];
+                for (int i = a.length - 1, j = 0; i >= 0; i--, j++) {
+                    newArr[j] = a[i];
+
+                }
+                for (int i = 0; i < newArr.length; i++)
+                    System.out.print(newArr[i] + " ");
+                System.out.println("\n");
+            }
+            public static void main(String[] args){
+                reverse();
+            }
 }
