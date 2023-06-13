9d4691df28e03f6c1d4eafe9aeed8525168a6e26
Divya <divyareddy7390@gmail.com>
2023-06-13 15:39:52 -0500

    commit this

--- Fib2/src/week2/Excep.java
+++ Fib2/src/week2/Excep.java
@@ -1,22 +1,21 @@
 package week2;
 
 public class Excep {
-    public static int stringtoint(String s2) {
-        int number = Integer.parseInt(s2);
-        System.out.println(number);
-        return number;
-    }
-    public static void main(String[] args)
-    {
-        try
-        {
-            stringtoint("123");
-            stringtoint("test");
-        }
-        catch (NumberFormatException e)
-        {
+    public static void stringtoint(String s2) {
+        try {
+            int number = Integer.parseInt(s2);
+            System.out.println(number);
+        } catch (NumberFormatException e) {
             System.out.println("Given String cannot be converted into Integer");
         }
+    }
+
+    public static void main(String[] args) {
+
+        stringtoint("23");
+        stringtoint("45.67");
+        stringtoint("test");
+        stringtoint("21f");
 
     }
 }
