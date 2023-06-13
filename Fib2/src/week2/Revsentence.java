7b327319d32341e2d05d862988c7617855dca5dc
Divya <divyareddy7390@gmail.com>
2023-06-13 15:23:18 -0500

    commit this

--- Fib2/src/week2/Revsentence.java
+++ Fib2/src/week2/Revsentence.java
@@ -6,17 +6,15 @@ public class Revsentence {
         String str="Java J2EE Reverse Me";
         String a[]=str.split(" ");
         String s2=" ";
-        //for(i=0;i<a.length-1;i++)
         int i=0,j;
-         for( i=0;i<=a.length-1;i++)//i=3
+         for( i=0;i<=a.length-1;i++)
          {
              s2 = a[i];
              s2=s2+" ";
-             for (j = s2.length() - 1; j >= 0; j--)//j=4
+             for (j = s2.length() - 1; j >= 0; j--)
              {
                  System.out.print(s2.charAt(j));
              }
-
          }
     }
 }
---
+++ Fib2/src/week2/TestCustomException1.java
@@ -0,0 +1,35 @@
+package week2;
+class InvalidAgeException  extends Exception
+{
+    public InvalidAgeException (String str)
+    {
+        super(str);
+    }
+}
+public class TestCustomException1
+{
+    static void validate (int age) throws InvalidAgeException{
+        if(age < 18){
+            throw new InvalidAgeException("age is not valid to vote");
+        }
+        else {
+            System.out.println("welcome to vote");
+        }
+    }
+    public static void main(String args[])
+    {
+        try
+        {
+            validate(13);
+        }
+        catch (InvalidAgeException ex)
+        {
+
+            System.out.println("Exception occured: " + ex);
+        }
+        finally
+        {
+            System.out.println("Executing something");
+        }
+    }
+}
