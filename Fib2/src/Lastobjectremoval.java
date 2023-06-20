de263c9314f69fbd58e26e4647baa8bc730dc41a
Divya <divyareddy7390@gmail.com>
2023-06-20 15:46:16 -0500

    commit this file

---
+++ Fib2/src/Lastobjectremoval.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Lastobjectremoval {
+    public static void main(String args[])
+    {
+        ArrayList<String> obj=new ArrayList<String>();
+        obj.add("Harini");
+        obj.add("Barini");
+        obj.add("Dharani");
+        Collections.sort(obj);
+        System.out.println(obj.remove(obj.size()-1));
+    }
+}
