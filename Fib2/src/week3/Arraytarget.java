262b4b5cdeb50ce1cde87e5960cb11242fb181b1
Divya <divyareddy7390@gmail.com>
2023-06-22 15:16:56 -0500

    commit this distinct chars and array target sum

---
+++ Fib2/src/week3/Arraytarget.java
@@ -0,0 +1,17 @@
+package week3;
+
+public class Arraytarget {
+
+    public static void main(String args[]) {
+        int[] arr = {10, 20, 30, 40, 50, 60};
+        int target = 50;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    System.out.println("firstno:" +  i + "\t" +"secondno: " +""+ j + "\t"+arr[i] + " "+ arr[j]);
+                }
+            }
+        }
+    }
+}
