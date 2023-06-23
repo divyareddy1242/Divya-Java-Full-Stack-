5760088c582072f2c0cd43609b2321ae34a7fbd2
Divya <divyareddy7390@gmail.com>
2023-06-23 15:11:21 -0500

    commit this files

---
+++ Fib2/src/week3/Hashset.java
@@ -0,0 +1,25 @@
+package week3;
+
+import java.util.HashSet;
+import java.util.Iterator;
+public class Hashset {
+    public static void main(String[] args) {
+        HashSet<String> names = new HashSet<String>();
+
+        names.add("Albert");
+        names.add("Anu");
+        names.add("Niha");
+        names.add("Niha");
+        names.add("Divi");
+        System.out.println(names);
+        Iterator<String> itr = names.iterator();
+        System.out.println("Printing names using iterator: ");
+        while (itr.hasNext()) {
+            System.out.print(itr.next() + " ");
+        }
+        System.out.println("\n" + "printing names using ForEach Loop: ");
+        names.forEach(name -> System.out.print(name + " "));
+        System.out.println("\n" + "Printing names using Streams ForEach is: ");
+        names.stream().forEach(name -> System.out.print(name + " "));
+    }
+}
---
+++ Fib2/src/week3/Hashsetimplementation.java
@@ -0,0 +1,12 @@
+package week3;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+public class Hashsetimplementation {
+
+        public static void main(String[] args)
+        {
+            String name="Divyaie";
+            HashSet<Character> set = name.chars().mapToObj(e->(char) e).collect(Collectors.toCollection(HashSet::new));
+            System.out.println(set);
+        }
+}
